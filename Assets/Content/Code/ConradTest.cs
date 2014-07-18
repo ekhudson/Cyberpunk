03c5f77d0327fb513f4c9d471bea835a1a2199f1
ekhudson <ekhudson@gmail.com>
2014-07-17 22:29:41 -0700

    +Walk Test +Sprite Lighting Test

---
+++ Assets/Content/Code/ConradTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConradTest : MonoBehaviour 
+{
+	private Animator mAnimator;
+
+	// Use this for initialization
+	private void Start () 
+	{
+		mAnimator = GetComponent<Animator> ();	
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			mAnimator.SetTrigger("GoToWalk");
+		}
+
+		if (Input.GetKeyUp (KeyCode.A)) 
+		{
+			mAnimator.SetTrigger("GoToIdle");
+		}
+	}
+}
