1c3d29808d1fb0dbf26b0006d43a4774eef5c7e8
Elliot Hudson <elliot@blackbirdinteractive.com>
2014-11-14 13:02:23 -0800

    +Coin throwing ++

--- Assets/Standard Assets/Scripts/Camera Scripts/PlayerScript.cs
+++ Assets/Standard Assets/Scripts/Camera Scripts/PlayerScript.cs
@@ -7,40 +7,99 @@ public class PlayerScript : MonoBehaviour
     public BoxCollider CoinBounds;
     public LayerMask CoinBoundsLayerMask;
     public float CoinForce = 25f;
+    public Vector3 PlayerCoinStartingRotation = new Vector3(90f, 0f, 0f);
+    public float PlayerCoinDistanceFromCamera = 10f;
+    public float ShotDelay = 5f;
+    public Vector3 ShotReloadOffset = new Vector3(0.1f, 0.1f, 0f);
+
 
     private Ray mRay;
     private RaycastHit mHit;
 
     private GameObject mPreviewCoin;
     private Renderer mPreviewRenderer;
+    private float mCountHeight = 0f;
+    private Camera mCamera;
+    private GameObject mInventoryCoin;
+
+    private float mCurrentReloadTime = 0f;
+    private Vector3 mCurrentReloadPosition = Vector3.zero;
 
     private void Start()
     {
+        mCamera = Camera.main;
         mPreviewCoin = (GameObject)GameObject.Instantiate(PlayerCoinPrefab);
-        mPreviewRenderer = mPreviewCoin.GetComponentInChildren<MeshRenderer>();
-        mPreviewCoin.renderer.enabled = false;
-        mPreviewRenderer.enabled = false;
+        mPreviewRenderer = mPreviewCoin.GetComponent<MeshRenderer>();
+
+        foreach (Material mat in mPreviewRenderer.materials)
+        {
+            mat.color = Color.Lerp(mat.color, Color.clear, 0.45f);
+        }
+     
         mPreviewCoin.rigidbody.isKinematic = true;
+        mPreviewCoin.transform.rotation = Quaternion.Euler(PlayerCoinStartingRotation);
+        mCountHeight = mPreviewRenderer.bounds.extents.y;
+        mCurrentReloadPosition = mCamera.ViewportToWorldPoint(ShotReloadOffset);
+
+        mInventoryCoin = (GameObject)GameObject.Instantiate(PlayerCoinPrefab);
+        mInventoryCoin.rigidbody.isKinematic = true;
+        mInventoryCoin.collider.enabled = false;
+        mInventoryCoin.transform.position = mCamera.ViewportToWorldPoint(ShotReloadOffset);
+   
     }
 
-	private void Update()
+	private void LateUpdate()
     {
-        mRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-        mHit = new RaycastHit();
+       // mRay = new Ray(mCamera.transform.position, mCamera.transform.forward);
+       // mHit = new RaycastHit();
 
-        if (Physics.Raycast(mRay, out mHit, 100f, CoinBoundsLayerMask))
+//        if (Physics.Raycast(mRay, out mHit, 100f, CoinBoundsLayerMask))
+//        {
+//            Vector3 point = mHit.point;
+//            point.y = mHit.collider.transform.position.y + mCountHeight;
+//            mPreviewCoin.transform.position = point;
+//            mPreviewCoin.transform.forward = Camera.main.transform.forward;
+//            mPreviewCoin.transform.Rotate(PlayerCoinStartingRotation);
+//            mPreviewRenderer.enabled = true;
+//        }
+
+        DrawInventory();
+
+        if (mCurrentReloadTime < ShotDelay)
         {
-            Vector3 point = mHit.point;
-            point.y = mHit.collider.transform.position.y;
-            mPreviewCoin.transform.position = point;
-            mPreviewCoin.transform.forward = mHit.normal;
-            mPreviewRenderer.enabled = true;
+            mCurrentReloadTime += Time.deltaTime;
+
+            float time = mCurrentReloadTime / ShotDelay;
+
+            mPreviewCoin.transform.position = Vector3.Slerp(mCurrentReloadPosition, mCamera.transform.position + (mCamera.transform.forward * PlayerCoinDistanceFromCamera), time);
+            mPreviewRenderer.transform.forward = Vector3.Slerp(Vector3.down, mCamera.transform.forward, time);
+            mPreviewCoin.transform.rotation = Quaternion.Slerp(Quaternion.Euler(-PlayerCoinStartingRotation), Quaternion.Euler(PlayerCoinStartingRotation), time);
+            return;
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        mPreviewCoin.transform.position = (mCamera.transform.position + (mCamera.transform.forward * PlayerCoinDistanceFromCamera));
+        mPreviewCoin.transform.forward = mCamera.transform.forward;
+        mPreviewCoin.transform.Rotate(PlayerCoinStartingRotation);
+
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
         {
             SpawnCoin();
+            Reload();
         }
+    }
+
+    private void DrawInventory()
+    {
+        mInventoryCoin.transform.position = mCamera.ViewportToWorldPoint(ShotReloadOffset);
+        mInventoryCoin.transform.forward = mCamera.transform.forward;
+        mInventoryCoin.transform.Rotate(-PlayerCoinStartingRotation);
+    }
+
+    private void Reload()
+    {
+        mCurrentReloadTime = 0;
+        mCurrentReloadPosition = mCamera.ViewportToWorldPoint(ShotReloadOffset);
+        mPreviewCoin.transform.position = mCurrentReloadPosition;
     }
 
     private void SpawnCoin()
@@ -48,8 +107,8 @@ public class PlayerScript : MonoBehaviour
         Vector3 pos = mPreviewCoin.transform.position;
         Quaternion rot = mPreviewCoin.transform.rotation;
 
-        GameObject playerCoin = (GameObject)GameObject.Instantiate(PlayerCoinPrefab, pos, Quaternion.identity);
+        GameObject playerCoin = (GameObject)GameObject.Instantiate(PlayerCoinPrefab, pos, rot);
 
-        playerCoin.rigidbody.AddRelativeForce(Camera.main.transform.forward * CoinForce, ForceMode.VelocityChange);
+        playerCoin.rigidbody.AddForceAtPosition(mCamera.transform.forward * CoinForce, playerCoin.transform.position, ForceMode.VelocityChange);
     }
 }
